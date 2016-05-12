8030a46ce56814ff2a613f83d0f40e9e18d30235
Alexander Lazarević <Alexander.Lazarevic@external.t-systems.com>
2016-05-12 15:58:10 +0200

    Added gradient descent for multiple features

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -7,6 +7,7 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
+for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
@@ -15,15 +16,8 @@ J_history = zeros(num_iters, 1);
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-n = length(theta);
-for iter = 1:num_iters
-  delta = X * theta - y;
-  newtheta = zeros(n, 1);
-  for j = 1:n
-    newtheta(j) = theta(j) - alpha / m * sum(delta .* X(:,j))
-  end
-  theta = newtheta
-  input("ok")
+theta = theta - (sum((X * theta - y) .* X) * alpha / m)';
+
 
 
 
@@ -36,7 +30,7 @@ for iter = 1:num_iters
     % ============================================================
 
     % Save the cost J in every iteration    
-    % J_history(iter) = computeCostMulti(X, y, theta);
+    J_history(iter) = computeCostMulti(X, y, theta);
 
 end
 
